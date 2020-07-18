3e18a1864830e104ac083d6134b2f677eeb426ba
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-07-18 12:31:05 +0300

    Инит привязок

--- CompManBase/MainWindow.xaml.cs
+++ CompManBase/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using CompManBase.WinModels;
+using System.Windows;
+using System.Windows.Data;
 
 namespace CompManBase
 {
@@ -7,9 +9,22 @@ namespace CompManBase
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowPanel _panel = new MainWindowPanel();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowPanel.DataContext = _panel;
+            Binding bindingTitle = new Binding
+            {
+                Source = _panel,
+                Path = new PropertyPath("Title"),
+            };
+            this.SetBinding(TitleProperty, bindingTitle);
+
+
         }
     }
 }
---
+++ CompManBase/WinModels/MainWindowPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompManBase.WinModels
+{
+    public class MainWindowPanel : INotifyPropertyChanged
+    {
+        private string _title;
+
+
+
+
+        #region Свойства-зависимости
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value == _title) return;
+                _title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+        #endregion
+        public MainWindowPanel()
+        {
+            
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
